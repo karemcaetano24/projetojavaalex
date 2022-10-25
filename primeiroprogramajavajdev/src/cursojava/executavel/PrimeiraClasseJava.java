c44eaec19b577722451652c712c8db6bb1715237
Karem Caetano <karemcaetano@gmail.com>
2022-10-25 09:42:37 -0300

    Criação do projeto

---
+++ primeiroprogramajavajdev/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,11 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	/*Main é um metodo auto executavel em Java*/
+	public static void main(String[] args) {
+
+		/*Escreve texto no console*/
+	System.out.println("Meu primeiro programa em Java - do Curso Formação Java Web ");
+	System.out.println("Java é orientado a objetos ");
+}
+}
