d26fbb8a24e4419b0a152e3c7e4c90ad5a29b1e2
Karem Caetano <karemcaetano@gmail.com>
2022-11-06 00:51:54 -0300

    Alteração e criação da classe pessoa

---
+++ primeiroprogramajavajdev/src/curso/java/alex/Pessoa.java
@@ -0,0 +1,16 @@
+package curso.java.alex;
+
+public class Pessoa {
+
+	private String nome;
+	
+	private String sobrenome;
+	
+	public String getNome() {
+		return nome;	
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
